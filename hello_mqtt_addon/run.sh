72312fcf083dc6dfb43e6bdc48b02ec0d097ae85
sunnyhd <hd.sunny4@gmail.com>
2024-07-17 10:39:25 +0200

    hello_mqtt

---
+++ hello_mqtt_addon/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Load configuration from options
+MQTT_BROKER=${MQTT_BROKER:-"localhost"}
+MQTT_PORT=${MQTT_PORT:-1883}
+MQTT_USERNAME=${MQTT_USERNAME:-""}
+MQTT_PASSWORD=${MQTT_PASSWORD:-""}
+MQTT_TOPIC=${MQTT_TOPIC:-"homeassistant/test"}
+MESSAGE=${MESSAGE:-"Hello from Home Assistant add-on"}
+
+# Export the variables for use in the Python script
+export MQTT_BROKER
+export MQTT_PORT
+export MQTT_USERNAME
+export MQTT_PASSWORD
+export MQTT_TOPIC
+export MESSAGE
+
+# Start the Python script
+python3 /app/hello_mqtt.py
